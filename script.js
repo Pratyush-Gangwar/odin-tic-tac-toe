87bc9ce91cf92f7f1c2fe8936adfa9208fbcb6f0
Pratyush-Gangwar <pratyush23395@iiitd.ac.in>
2026-01-25 17:40:04 +0530

    Added game session logic

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function Player(name, symbol) {
     this.symbol = symbol;
 }
 
-const game = (function() {
-    const board = Array.from({ length: 3 }, () => Array(3).fill('.'));
+function makeGameState() {
+    const board = Array.from({ length: 3 }, () => Array(3).fill('.')); // private
 
     function hasPlayerWonRow(player) {
         for(const row of board) {
@@ -73,7 +73,70 @@ const game = (function() {
         board[i][j] = symbol;
     }
 
+    function printBoard() {
+        for(const row of board) {
+            console.log(row);
+        }
+    }
+
     return {
-        hasPlayerWon, place
+        hasPlayerWon, place, printBoard
     };
-})();
+};
+
+function makeGameSession() {
+
+    function getPlayers() {
+        const name1 = prompt("Player 1, enter your name: ");
+        const symbol1 = prompt("Player 1, enter your symbol (X/O): ");
+        const player1 = new Player(name1, symbol1);
+
+        const name2 = prompt("Player 2, enter your name: ");
+        const symbol2 = (symbol1 === 'O' ? 'X' : 'O');
+        const player2 = new Player(name2, symbol2);
+
+        return {player1, player2};
+    }
+
+    function start() {
+        const players = getPlayers();
+        const gameState = makeGameState();
+
+        let moves = 0;
+        let gameFinished = false;
+
+        while (!gameFinished) {
+            
+            for(const player of Object.values(players)) {
+                gameState.printBoard();
+
+                const row = prompt(`Player ${player.name}, enter your row: `);
+                const col = prompt(`Player ${player.name}, enter your col: `);
+
+                gameState.place(row, col, player.symbol);
+                moves++;
+
+                if (gameState.hasPlayerWon(player)) {
+                    gameState.printBoard();
+                    console.log(`Player ${player.name} won!`);
+                    gameFinished = true;
+                    break;
+                }
+
+                else if (moves == 9) {
+                    gameState.printBoard();
+                    console.log("It's draw!");
+                    gameFinished = true;
+                    break;
+                }
+
+            }
+        }
+    }
+
+    return {start};
+
+};
+
+const gameSession = makeGameSession();
+gameSession.start();
